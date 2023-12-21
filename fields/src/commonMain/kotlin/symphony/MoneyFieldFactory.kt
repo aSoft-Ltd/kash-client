bf683d16b8f30e0d3f70d1aa7fcfbd38f4115614
andylamax <andylamax@programmer.net>
2023-12-22 02:59:53 +0300

    Migrated to K2 and primitive collections

--- fields/src/commonMain/kotlin/symphony/MoneyFieldFactory.kt
+++ fields/src/commonMain/kotlin/symphony/MoneyFieldFactory.kt
@@ -5,9 +5,10 @@ import kotlin.reflect.KMutableProperty0
 
 fun Fields<*>.money(
     name: KMutableProperty0<Double?>,
+    value: Double? = name.get(),
     label: String = name.name,
     hint: String = label,
     visibility: Visibility = Visibilities.Visible,
     onChange: Changer<Double>? = null,
     factory: ValidationFactory<Double>? = null
-): NumberField<Double> = double(name, label, hint, visibility, onChange, factory)
+): NumberField<Double> = double(name, value, label, hint, visibility, onChange, factory)
